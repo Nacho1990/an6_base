83997ccda73ab89b839d026cf59e8875bf918623
Nacho Criado <nachocriado9@gmail.com>
2018-05-09 11:55:11 -0300

    pagina finalizada para probar en prod

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+// import { resolve } from 'dns';
+// import { reject } from 'q';
 
 @Injectable()
 export class ProductosService {
   
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
   constructor( private http:Http) { 
@@ -12,18 +15,53 @@ export class ProductosService {
     this.cargar_productos();    
   }
 
+  public buscar_producto ( termino:string ){
+
+    if( this.productos.length === 0){
+      this.cargar_productos().then(()=>{
+        //termino la carga
+        this.filtrar_productos(termino);
+      });
+    }else{
+      this.filtrar_productos(termino);
+    }
+
+  }
+  
+  private filtrar_productos(termino:string){
+    this.productos_filtrado =[];
+    //paso termino a minusculo
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod =>{
+
+      if(prod.categoria.indexOf(termino)>=0 || prod.titulo.toLowerCase().indexOf(termino)>=0){
+        this.productos_filtrado.push( prod);
+      }
+
+      // console.log(prod);
+    })
+
+  }
+
   public cargar_producto(cod:string){
     return this.http.get(`https://paginaweb-d9478.firebaseio.com/productos/${ cod }.json`)
   }
+  
   public cargar_productos(){
+    this.cargando = true;
+    
+    let promesa = new Promise((resolve, reject)=>{
 
-    this.cargando = true;      
-    this.http.get('https://paginaweb-d9478.firebaseio.com/productos_idx.json')
-        .subscribe( res =>{
-          console.log(res.json());
-          this.cargando = false;
-          this.productos = res.json();
-        })
+      this.http.get('https://paginaweb-d9478.firebaseio.com/productos_idx.json')
+          .subscribe( res =>{
+            //console.log(res.json());
+            this.cargando = false;
+            this.productos = res.json();
+            resolve();
+        });
+    });
+    return promesa;   
     
   }
 }
