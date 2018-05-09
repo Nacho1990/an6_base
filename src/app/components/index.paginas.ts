ea28e23675025f8cd2c897a24e3dae83af64707c
Nacho Criado <nachocriado9@gmail.com>
2018-05-09 10:36:36 -0300

    pagina search creada

--- src/app/components/index.paginas.ts
+++ src/app/components/index.paginas.ts
@@ -1,4 +1,5 @@
+//listado de rutas de paginas
 export { AboutComponent } from "./about/about.component";
 export { PortafolioComponent } from "./portafolio/portafolio.component";
 export { ProductoComponent } from "./producto/producto.component";
-
+export { SearchComponent } from "./search/search.component";
